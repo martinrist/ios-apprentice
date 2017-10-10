17cc68fdd901c54eea768063d07c893a145260e4
Martin Rist <mlr-github@martinrist.com>
2017-10-10 17:51:20 +0100

    Add 'About' page with instructions

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Martin Rist on 10/10/2017.
+//  Copyright © 2017 Martin Rist. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
