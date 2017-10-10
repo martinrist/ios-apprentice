620c7f9799aa2d66953d7695e018b8a4decfc88a
Martin Rist <mlr-github@martinrist.com>
2017-10-10 14:39:36 +0100

    Add initial button with alert popup

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World!", message: "This is my first app", preferredStyle: .alert)
+        let awesomeAction = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(awesomeAction)
+        let notAwesomeAction = UIAlertAction(title: "NotAwesome", style: .destructive, handler: nil)
+        alert.addAction(notAwesomeAction)
+        
+        let thirdAction = UIAlertAction(title: "Number 3", style: .default, handler: nil)
+        alert.addAction(thirdAction)
+        
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
 
 }
 
