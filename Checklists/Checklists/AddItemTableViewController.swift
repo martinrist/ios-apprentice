676d2c0f27f944b664e85f4052fe431031240abe
Martin Rist <mlr-github@martinrist.com>
2017-10-22 17:40:24 +0100

    Tidy UI for 'Add item' screen

--- Checklists/Checklists/AddItemTableViewController.swift
+++ Checklists/Checklists/AddItemTableViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,24 @@ class AddItemTableViewController: UITableViewController {
 
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
     }
 
 }
