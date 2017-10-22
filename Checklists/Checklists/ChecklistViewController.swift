0f5389627f56c87479302d21766cc38dc6bdee98
Martin Rist <mlr-github@martinrist.com>
2017-10-22 16:10:04 +0100

    Add 5 custom tasks with hard-coded values

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -21,11 +21,28 @@ class ChecklistViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        switch indexPath.row {
+        case 0:
+            label.text = "Walk the dog"
+        case 1:
+            label.text = "Brush my teeth"
+        case 2:
+            label.text = "Learn iOS development"
+        case 3:
+            label.text = "Soccer practice"
+        case 4:
+            label.text = "Eat ice cream"
+        default:
+            label.text = ""
+        }
+
         return cell
     }
 }
