40ea570a31ae78c7505d91d56104a9f893a20c33
Martin Rist <mlr-github@martinrist.com>
2017-11-07 14:14:05 +0000

    Implement checklist editing

--- Checklists/Checklists/AllListsViewController.swift
+++ Checklists/Checklists/AllListsViewController.swift
@@ -71,6 +71,15 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         }
     }
 
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+
+        navigationController?.pushViewController(controller, animated: true)
+    }
 
     //MARK:- AllListsViewController delegate
     func listDetailViewControllerDidCancel(_ controller: ListDetailViewController) {
