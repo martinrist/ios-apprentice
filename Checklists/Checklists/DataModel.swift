97121cad5ab04c2c6b2fc798c8da999b8c7e6072
Martin Rist <mlr-github@martinrist.com>
2017-11-09 12:41:37 +0000

    Add new Checklist on first load

--- Checklists/Checklists/DataModel.swift
+++ Checklists/Checklists/DataModel.swift
@@ -14,10 +14,12 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
 
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1 ]
+        let dictionary: [String:Any] = [ "ChecklistIndex": -1,
+                           "FirstTime": true ]
         UserDefaults.standard.register(defaults: dictionary)
     }
 
@@ -57,6 +59,19 @@ class DataModel {
             } catch {
                 print("Error decoding item array!")
             }
+        }
+    }
+
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
         }
     }
 
