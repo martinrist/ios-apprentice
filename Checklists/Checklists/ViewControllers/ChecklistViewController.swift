22e024a3eef0e300b6454c31d546fcc057e08f5b
Martin Rist <mlr-github@martinrist.com>
2017-11-10 08:54:58 +0000

    Add tints to nav bar and checkmark

--- Checklists/Checklists/ViewControllers/ChecklistViewController.swift
+++ Checklists/Checklists/ViewControllers/ChecklistViewController.swift
@@ -44,6 +44,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         
         let label = cell.viewWithTag(1001) as! UILabel
+        label.textColor = view.tintColor
         if item.checked {
             label.text = "✓"
         } else {
