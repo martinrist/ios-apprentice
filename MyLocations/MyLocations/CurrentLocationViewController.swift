037572b023caeab1714090b7648c81d39c317836
Martin Rist <mlr-github@martinrist.com>
2017-11-12 14:06:31 +0000

    Start listening for location

--- MyLocations/MyLocations/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
+class CurrentLocationViewController: UIViewController,
+                                     CLLocationManagerDelegate {
+
+    // MARK:- Properties
+    let locationManager = CLLocationManager()
+
 
     // MARK:- Outlets
     
@@ -23,9 +29,34 @@ class CurrentLocationViewController: UIViewController {
     // MARK:- Actions
     
     @IBAction func getLocation() {
-        // do nothing yet
+
+        let authStatus = CLLocationManager.authorizationStatus()
+
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+
+        if authStatus == .denied {
+            showLocationServicesDeniedAlert()
+            return
+        }
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
-    
+
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(title: "Location Services Disabled",
+                                      message: "Please enable location services for this app in Settings.",
+                                      preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
     
     // MARK:- View Lifecycle
     
@@ -38,6 +69,21 @@ class CurrentLocationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    // MARK:- CLLocationManagerDelegate
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("didUpdateLocation \(newLocation)")
+    }
+
+
+    // MARK:- Miscellaneous Functions
 
 
 }
