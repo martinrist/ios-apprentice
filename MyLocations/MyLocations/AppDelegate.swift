009c68d1630f0866385570228afb43228a73cb2e
Martin Rist <mlr-github@martinrist.com>
2017-11-19 12:25:13 +0000

    Add error handling code for CoreData errors

--- MyLocations/MyLocations/AppDelegate.swift
+++ MyLocations/MyLocations/AppDelegate.swift
@@ -42,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             controller.managedObjectContext = managedObjectContext
         }
         print(applicationDocumentsDirectory)
+        listenForFatalCoreDataNotifications()
         return true
     }
 
@@ -67,6 +68,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // MARK:- Helper methods
+    func listenForFatalCoreDataNotifications() {
+        NotificationCenter.default.addObserver(forName: CoreDataSaveFailedNotification,
+                                               object: nil,
+                                               queue: OperationQueue.main,
+                                               using: { notification in
+                                                let message = """
+There was a fatal error in the app and it cannot continue.
 
+Press OK to terminate the app.  Sorry for the inconvenience.
+"""
+
+                                                let alert = UIAlertController(title: "Internal Error",
+                                                                              message: message,
+                                                                              preferredStyle: .alert)
+
+                                                let action = UIAlertAction(title: "OK",
+                                                                           style: .default) { _ in
+                                                                            let exception = NSException(name: NSExceptionName.internalInconsistencyException, reason: "Fatal Core Data error", userInfo: nil)
+                                                                            exception.raise()
+                                                                            }
+                                                alert.addAction(action)
+
+                                                let tabController = self.window!.rootViewController!
+                                                tabController.present(alert, animated: true, completion: nil)
+                                                })
+    }
 }
 
--- MyLocations/MyLocations/LocationDetailsViewController.swift
+++ MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -61,7 +61,7 @@ class LocationDetailsViewController: UITableViewController {
                 self.navigationController?.popViewController(animated: true)
             }
         } catch {
-            fatalError("Error: \(error)")
+            fatalCoreDataError(error)
         }
     }
 
