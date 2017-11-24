84de36aaec8c8c4e9382f4b472f6c0b49c4cacac
Martin Rist <mlr-github@martinrist.com>
2017-11-24 18:35:13 +0000

    Add custom LocationCell class

--- MyLocations/MyLocations/LocationsViewController.swift
+++ MyLocations/MyLocations/LocationsViewController.swift
@@ -47,29 +47,14 @@ class LocationsViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell",
+                                                 for: indexPath)
+                        as! LocationCell
 
         let location = locations[indexPath.row]
 
-        let descriptionLabel = cell.viewWithTag(100) as! UILabel
-        descriptionLabel.text = location.locationDescription
-
-        let addressLabel = cell.viewWithTag(101) as! UILabel
-        if let placemark = location.placemark {
-            var text = ""
-            if let s = placemark.subThoroughfare {
-                text += s + " "
-            }
-            if let s = placemark.thoroughfare {
-                text += s + ", "
-            }
-            if let s = placemark.locality {
-                text += s
-            }
-            addressLabel.text = text
-        } else {
-            addressLabel.text = ""
-        }
+        cell.configure(for: location)
 
         return cell
     }
