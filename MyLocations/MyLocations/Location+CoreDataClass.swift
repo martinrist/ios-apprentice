fe379fbb301ff4c744545945b9a8f3c408e58f73
Martin Rist <mlr-github@martinrist.com>
2017-12-13 13:42:18 +0000

    Delete image file on location deletion

--- MyLocations/MyLocations/Location+CoreDataClass.swift
+++ MyLocations/MyLocations/Location+CoreDataClass.swift
@@ -51,4 +51,14 @@ public class Location: NSManagedObject, MKAnnotation {
         userDefaults.synchronize()
         return currentID
     }
+
+    func removePhotoFile() {
+        if hasPhoto {
+            do {
+                try FileManager.default.removeItem(at: photoURL)
+            } catch {
+                print("Error removing file: \(error)")
+            }
+        }
+    }
 }
