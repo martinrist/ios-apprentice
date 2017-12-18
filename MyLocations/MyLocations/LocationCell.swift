f6b3911af5b333740f370887cbdece6d648d0ffc
Martin Rist <mlr-github@martinrist.com>
2017-12-18 15:52:55 +0000

    Make images circular

--- MyLocations/MyLocations/LocationCell.swift
+++ MyLocations/MyLocations/LocationCell.swift
@@ -21,6 +21,11 @@ class LocationCell: UITableViewCell {
         let selection = UIView(frame: CGRect.zero)
         selection.backgroundColor = UIColor(white: 1.0, alpha: 0.2)
         selectedBackgroundView = selection
+
+        // Rounded corners for image
+        photoImageView.layer.cornerRadius = photoImageView.bounds.size.width / 2
+        photoImageView.clipsToBounds = true
+        separatorInset = UIEdgeInsets(top: 0, left: 82, bottom: 0, right: 0)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
