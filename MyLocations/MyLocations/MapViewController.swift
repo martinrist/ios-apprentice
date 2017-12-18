85fc1c69207db78f27ad2832c822bf05b4d5006b
Martin Rist <mlr-github@martinrist.com>
2017-12-18 15:38:26 +0000

    Tidy colours on Map View

--- MyLocations/MyLocations/MapViewController.swift
+++ MyLocations/MyLocations/MapViewController.swift
@@ -138,6 +138,7 @@ extension MapViewController: MKMapViewDelegate {
             pinView.canShowCallout = true
             pinView.animatesDrop = false
             pinView.pinTintColor = UIColor(red: 0.32, green: 0.82, blue: 0.4, alpha: 1)
+            pinView.tintColor = UIColor(white: 0.0, alpha: 0.5)
 
             let rightButton = UIButton(type: .detailDisclosure)
             rightButton.addTarget(self, action: #selector(showLocationDetails), for: .touchUpInside)
