c0bf891d8aea085b8047d6b7498be25c10f7b310
Martin Rist <mlr-github@martinrist.com>
2017-12-19 12:37:56 +0000

    Update colours on Category Picker

--- MyLocations/MyLocations/CategoryPickerViewController.swift
+++ MyLocations/MyLocations/CategoryPickerViewController.swift
@@ -59,6 +59,11 @@ class CategoryPickerViewController: UITableViewController {
         } else {
             cell.accessoryType = .none
         }
+
+        let selection = UIView(frame: CGRect.zero)
+        selection.backgroundColor = UIColor(white: 1.0, alpha: 0.2)
+        cell.selectedBackgroundView = selection
+        
         return cell
     }
 
