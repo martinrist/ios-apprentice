d66745b5fe16c4627b0497468d1a92614b958906
Martin Rist <mlr-github@martinrist.com>
2018-01-01 18:48:35 +0000

    Ensure first row of table view is always visible

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -13,12 +13,13 @@ class SearchViewController: UIViewController {
     // MARK:- Outlets
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    
+
     // MARK:- View lifecycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+
     }
 
     override func didReceiveMemoryWarning() {
