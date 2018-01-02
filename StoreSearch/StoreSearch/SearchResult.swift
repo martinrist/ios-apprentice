ad9aad6a47e57fa15b27db04ab82f811311feca4
Martin Rist <mlr-github@martinrist.com>
2018-01-02 16:04:46 +0000

    Handle different metadata by product kind

--- StoreSearch/StoreSearch/SearchResult.swift
+++ StoreSearch/StoreSearch/SearchResult.swift
@@ -10,31 +10,55 @@ import Foundation
 
 class SearchResult: Codable, CustomStringConvertible {
 
-    var kind = ""
+    var kind: String?
     var artistName = ""
-    var trackName = ""
-    var trackPrice = 0.0
+    var trackName: String?
+    var trackPrice: Double?
     var currency = ""
     var imageSmall = ""
     var imageLarge = ""
-    var storeURL = ""
-    var genre = ""
+
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
 
     enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
     }
     
     var name: String {
-        return trackName
+        return trackName ?? collectionName ?? ""
     }
 
+    var storeURL: String {
+        return trackViewUrl ?? collectionViewUrl ?? ""
+    }
+
+    var price: Double {
+        return trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+        return ""
+    }
     var description: String {
-        return "Kind: \(kind), Name: \(name), Artist Name: \(artistName)"
+        return "Kind: \(kind ?? ""), Name: \(name), Artist Name: \(artistName)"
     }
 }
 
