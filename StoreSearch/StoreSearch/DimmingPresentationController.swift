37b12d4a6e14f2866d0d42e642704fc105cf0407
Martin Rist <mlr-github@martinrist.com>
2018-01-11 14:12:11 +0000

    Animate appearance of gradient view

--- StoreSearch/StoreSearch/DimmingPresentationController.swift
+++ StoreSearch/StoreSearch/DimmingPresentationController.swift
@@ -19,5 +19,20 @@ class DimmingPresentationController: UIPresentationController {
     override func presentationTransitionWillBegin() {
         dimmingView.frame = containerView!.bounds
         containerView!.insertSubview(dimmingView, at: 0)
+
+        dimmingView.alpha = 0
+        if let coordinator = presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 1
+            }, completion: nil)
+        }
+    }
+
+    override func dismissalTransitionWillBegin() {
+        if let coordinator = presentedViewController.transitionCoordinator {
+            coordinator.animate(alongsideTransition: { _ in
+                self.dimmingView.alpha = 0
+            }, completion: nil)
+        }
     }
 }
