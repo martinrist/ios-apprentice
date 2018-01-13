e7ba784450dfdfe861f906d79c3190b0987a59dd
Martin Rist <mlr-github@martinrist.com>
2018-01-13 16:34:48 +0000

    Add UIScrollView to landscape screen

--- StoreSearch/StoreSearch/LandscapeViewController.swift
+++ StoreSearch/StoreSearch/LandscapeViewController.swift
@@ -10,26 +10,44 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
+    // MARK:- Outlets
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+
+
+    // MARK:- View lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Remove constraints from main view
+        view.removeConstraints(view.constraints)
+        view.translatesAutoresizingMaskIntoConstraints = true
+
+        // Remove constraints for page control
+        pageControl.removeConstraints(pageControl.constraints)
+        pageControl.translatesAutoresizingMaskIntoConstraints = true
+
+        // Remove constraints for scroll view
+        scrollView.removeConstraints(scrollView.constraints)
+        scrollView.translatesAutoresizingMaskIntoConstraints = true
+
+        scrollView.backgroundColor = UIColor(patternImage:
+            UIImage(named: "LandscapeBackground")!)
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        scrollView.frame = view.bounds
+
+        pageControl.frame = CGRect(x: 0,
+                                   y: view.frame.size.height - pageControl.frame.size.height,
+                                   width: view.frame.size.width,
+                                   height: pageControl.frame.size.height)
     }
-    */
 
 }
